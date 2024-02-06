3fc6f77a818684013040511fafcdea5255b0f102
richard smith <richard.smith1@engineering.digital.dwp.gov.uk>
2024-02-06 10:19:02 +0000

    Changing the service name from Simplifying to Manage Employer Deductions

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Employer Deductions Service',
+  serviceName: 'Manage Employer Deductionss',
 
   // Default port that prototype runs on
   port: '3000',
